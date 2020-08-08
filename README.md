f7dfc52c7241a8d100912ad70dbbb4cc4ed8843b
Jeff Robertson <40304377+jeffsrobertson@users.noreply.github.com>
2020-08-07 20:37:34 -0700

    Add files via upload

--- README.md
+++ README.md
@@ -1,6 +1,88 @@
-# cityscapes-unet
-Pytorch implementation of a U-Net-inspired neural network, for performing semantic segmentation on the Cityscapes dataset.
+# About
 
-I'm actively training this model/playing around with hyperparameters, and will update this with a better description/summary once I'm satisfied with the results.
+In this project I trained a neural network from scratch to perform semantic segmentation on urban driving scenes. The model was created using Pytorch, and trained on an AWS EC2 instance using 8 GPUs in parallel (the p2.8xlarge instance).
 
-Despite the underwhelming README doc, this repository is fully functional as is! Just make sure you've downloaded the Cityscapes dataset to the directory /data/Cityscapes/. After only 2 epochs is able to achieve > 70% pixel accuracy across 34 categories. I'm training the model on an AWS EC2 server (P series). It takes roughly 40 minutes per epoch when using 1 GPU, and 6 minutes per epoch on 8 GPUs in parallel.
+## Dependencies
+
+This project used Pytorch for training and Torchvision to access the Cityscapes dataset. These can be downloaded using pip, via the following commands:
+
+> pip install torch
+>
+> pip install torchvision
+
+It also uses matplotlib, numpy, and time, all of which are included in the standard library.
+
+## Dataset
+
+The model was trained on the Cityscapes dataset, which can be downloaded [here](https://www.cityscapes-dataset.com/downloads/) (you'll have to make a free account). Specifically, you need to download the files "gtFine_trainvaltest.zip", "gtCoarse.zip", and "leftImg8bit_trainvaltest.zip". 
+
+If you are training on a remote server (such as an AWS EC2 instance), downloading the dataset can be a bit trickier since you need to login to the website. This can be done using the following shell commands:
+
+> wget --keep-session-cookies --save-cookies=cookies.txt --post-data 'username=USERNAME&password=PASSWORD&submit=Login' https://www.cityscapes-dataset.com/login/
+
+> wget --load-cookies cookies.txt --content-disposition https://www.cityscapes-dataset.com/file-handling/?packageID=1
+
+In the first line, replace USERNAME and PASSWORD with your username and password for the site. In the second line, where it says "packageID=1", replace the last number with 1, 2, and 3 to download the three required datasets mentioned above. Credit goes to [this](https://towardsdatascience.com/download-city-scapes-dataset-with-script-3061f87b20d7) blogpost for the workaround.
+
+## The Model
+
+The model I used for the neural network is based on the famous U-Net architecture ([link](https://arxiv.org/abs/1505.04597)):
+
+![U-net](images/u-net-architecture.png)
+
+I initially started with an exact replica of U-Net but found its results were underwhelming, so I added batch normalization after every convolution layer and it achieved much better performance. The model uses convolution layers and max pooling for downsampling/feature extraction, with ReLU activations. It then uses transposed convolutions (with stride of 2) in order to upsample the network back to its original resolution (approximately). At each upsampling step, the model has a skip connection which concatenates features from a previous layer with the newly upsampled features. 
+
+Note that while the original U-Net architecture used 'valid' padding for its convolutions, I found no signicant difference in performance between padding techniques, so all the results that follow were trained using 'same' padding in order to preserve image size. More info on this can be found in the "Additional Case Studies" section.
+
+## How to Use
+
+### Training
+
+The file trainer.ipynb contains everything to train your own neural network from scratch. The hyperparameters that can be customized are all contained in the first couple cells. I used the Adam optimizer and a pixel-averaged cross-entropy loss function to train the network.
+
+I was having memory issues, so my code actually reduces the resolution by a factor of two when it loads the images. Other than this, the only data augmentation I used was to randomly horizontally flip half the images. You can add additional augmentation methods by editing the Cityscapes class within the dataset.py file. Just make sure you apply identical transforms to both the image and the segmentation label. 
+
+The model was trained on 8 GPUs in parallel using a p2.8xlarge EC2 instance on AWS, and takes roughly ~15 mins per epoch (with batch size of 8) to train. The code automatically lowers the learning rate by a factor of 2 every 10 epochs. I found that the model stops improving after 20 epochs. This comes out to around 5 hours of training if using AWS, or ~$35.
+
+### Testing
+
+If you're either too impatient or don't have the computational resources to train your own model, I provided my pretrained models in the checkpoints folder. You can evaluate the models using the test.ipynb notebook. Here is an example of a predicted segmentation from the validation set, with ground truth label for comparison:
+
+![results](images/results.png)
+
+At a glance, it appears to classify most of the image correctly. It struggles with narrower objects, such as the pole on the right (although, in fairness, that one's difficult to see even with the human eye).
+
+## Results/Observations
+
+![metrics](images/metrics.png)
+
+I tracked two metrics for evaluating my model: (1) a simple pixel-averaged accuracy, and (2) the intersection over union (IoU) for each class. As you can see in the above figure, the pixel accuracy achieved 80%+ after only a few epochs, and then was a slow crawl to ~85% before flattening out around the 20th epoch. Beyond 20 epochs, the training loss continued to decrease while the validation loss remained flat, indicating that the model was being overfit. Note that, other than horizontal image flipping, I made no effort to augment the traininig data, so it's very likely that with data augmentation and/or regularization this model could be trained even further.
+
+
+![classious](images/classious.png)
+
+The above figure shows the computed intersection over union (IoU) for each class. More common objects tend to have much better IoUs, which makes sense as the network has more training data to learn from. Additionally, smaller objects such as traffic lights, signs, bicycles tend to have worse IoUs, as these classes are impacted more by noise/small variations in boundary estimates. 
+
+Most of the images tend to be dominated by a few classes, such as sky, road, cars, and trees. These classes tend to have fairly simple features, and so the neural network is able to learn what they look like very quickly. This is why the overall pixel accuracy is able to hit 80%+ after only a few epochs, despite the fact that the IoU for most classes remains low. The model then spends the remainder of its training trying to learn the features of the more complex, smaller objects, such as bicycles, people, and street signs. I plan to experiment with a modified loss function, where it gives more weight to classes which make up less pixels in the image, in order to try and encourage the model to prioritize learning the smaller classes more quickly. But I've already burned through a lot of money on my AWS instance, so I'll leave that on my "to do" list for now. :-)
+
+## Additional Case Studies
+
+#### Effects of padding
+
+I found it strange that the original U-Net paper used 'valid' padding in its convolutional layers, as this results in a loss of roughly ~50% of the pixels around the edge of the image. So I performed a unit test where I trained two models. Each model's hyperparameters were exactly identical, except one had valid padding while the other had 'same' padding in order to preserve image size. A comparison of their validation accuracies can be seen below:
+
+![padding](images/padding.png)
+
+As you can see, there is virtually no difference between using valid padding vs same. Therefore I see no reason to use valid padding, as was done in the original U-Net architecture. 
+
+Note that, in light of the above observations, all the previous results shown were from the model trained with 'same' padding.
+
+#### Improving the loss function
+
+Leaving this as a "to do" item for now. I plan to play with the loss function, and see if I can build a custom loss function which is able to prioritize learning smaller objects over things like road, sky, etc. 
+
+## About me
+
+I'm a Physics PhD-turned-data-scientist, with an emphasis on computer vision and deep learning methods. If you have any questions about me, this project, or something else, feel free to reach out:
+
+* [linkedin](https://www.linkedin.com/in/jeffsrobertson/)
--- model.py
+++ model.py
@@ -2,22 +2,147 @@ from utils import *
 import torch
 from torch import nn
 import numpy as np
+import time
+from collections import OrderedDict
 
-def conv_x2_block(in_features, out_features, padding):
+
+def test(index, dataset, model):
+    """Runs a prediction on a single image from the provided dataset. Plots the original image, ground truth
+        segmentation labels, and the predicted segmentation from the model.
+
+    index: (int) Index of the image from the dataset that you want to plot.
+    dataset: (Pytorch dataset) Cityscapes TEST dataset, loaded from torchvision.
+    model: (Pytorch model) Trained neural network for semantic segmentation.
+    """
+
+    # Load raw  and segmented images
+    img, seg = dataset[index]
+    class_ids = dataset.class_ids
+
+    # Run through model
+    img = img.unsqueeze(0)
+    model.eval()
+    with torch.no_grad():
+        yhat = model(img).squeeze(0)  # dim (n_classes, height, width)
+    prediction = yhat.argmax(axis=0)  # dim (height, width)
+
+    # Have to trim edges off image, to match size of prediction
+    img = centercrop(img, size=prediction.shape)
+    img = img.squeeze(0)
+    img = np.transpose(np.array(img), axes=(1, 2, 0))
+    pred = np.array(prediction.cpu())
+    seg = seg.cpu().numpy()
+
+    # Plot raw and segmented images stacked vertically
+    fig, (ax1, ax2, ax3) = plt.subplots(3, sharex=True)
+    plt.subplots_adjust(hspace=0)
+    plt1 = ax1.imshow(img)
+    plt2 = ax2.imshow(seg, cmap='jet', vmin=0, vmax=33)
+    # ax2.text(200, 100, 'test', color='white')
+    plt3 = ax3.imshow(pred, cmap='jet', vmin=0, vmax=33)
+    ax1.set_yticklabels([])
+    ax2.set_yticklabels([])
+    ax3.set_yticklabels([])
+    ax3.set_xticklabels([])
+    ax1.set_ylabel('Image')
+    ax2.set_ylabel('Ground truth')
+    ax3.set_ylabel('Prediction')
+
+    # Add a colorbar with corresponding seg labels
+    if class_ids is not None:
+        seg_ids = np.unique(seg)
+        cbar = fig.colorbar(plt2, ax=[ax1, ax2, ax3])
+        labels = [class_ids[i] for i in seg_ids]
+        cbar.set_ticks(seg_ids)
+        cbar.set_ticklabels(labels)
+
+
+def train(model, dataloader, criterion, optimizer):
+    model.train()
+    acc, class_ious, loss = _run_model(model, dataloader, criterion, optimizer=optimizer, mode='train')
+    return acc, class_ious, loss
+
+
+def validate(model, dataloader, criterion):
+    model.eval()
+    with torch.no_grad():
+        acc, class_ious, loss = _run_model(model, dataloader, criterion, optimizer=None, mode='val')
+    return acc, class_ious, loss
+
+
+def _run_model(model, dataloader, criterion, optimizer=None, mode='train'):
+    if type(model) == nn.DataParallel:
+        num_classes = model.module.n_classes
+    else:
+        num_classes = model.n_classes
+
+    batch_time = AverageMeter()
+    losses = AverageMeter()
+    class_IoUs = ClassIoUMeter(input_size=(1, num_classes))
+    accuracy = AverageMeter()
+
+    start_time = time.time()
+    for i, (x, y) in enumerate(dataloader):
+        if torch.cuda.is_available():
+            x = x.to("cuda")
+            y = y.to("cuda")
+
+        yhat = model(x)
+        loss = criterion(yhat, y.long())
+        new_class_ious = calculate_IoU(yhat, y)
+        new_acc = calculate_accuracy(yhat, y)
+
+        # Backprop
+        if mode == 'train':
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+        # Update tracking parameters
+        batch_time.update(time.time() - start_time, n=x.shape[0])
+        losses.update(loss.item(), n=x.shape[0])
+        class_IoUs.update(new_class_ious)
+        accuracy.update(new_acc, n=x.shape[0])
+
+        # Print progress
+        if mode == 'train':
+            print(
+                '{num_batch}/{total_batches} batches complete. train_loss: {loss:.3f}. train_acc: {acc:.3f}. train_iou: {iou:.3f}. Time: {batch_time:.1f} sec. '.format(
+                    num_batch=i + 1,
+                    total_batches=len(dataloader),
+                    batch_time=batch_time.val,
+                    loss=losses.avg,
+                    acc=accuracy.avg, iou=class_IoUs.average_iou()), end='\r')
+        else:
+            print('Evaluating model. {num_batch}/{total_batches} batches complete.'.format(
+                num_batch=i + 1,
+                total_batches=len(dataloader)), end='\r')
+
+    avg_acc = accuracy.avg
+    avg_class_ious = class_IoUs.avg
+    avg_loss = losses.avg
+    print('')
+    return avg_acc, avg_class_ious, avg_loss
+
+
+def _conv_x2_block(in_features, out_features, padding):
     """Returns two convolutional layers, each backed by a batch normalization layer and ReLU activation"""
     pad = 1 if padding == 'same' else 0
-    
-    return nn.Sequential(nn.Conv2d(in_features, out_features, kernel_size=3, padding=pad), 
-                           nn.BatchNorm2d(out_features), 
-                           nn.ReLU(), 
-                           nn.Conv2d(out_features, out_features, kernel_size=3, padding=pad), 
-                           nn.BatchNorm2d(out_features), 
-                           nn.ReLU())
 
-def upsample_block(in_features, out_features):
-    return nn.Sequential(nn.ConvTranspose2d(in_features, out_features, kernel_size=2, stride=2), 
-                   nn.BatchNorm2d(out_features), 
-                   nn.ReLU())
+    return nn.Sequential(nn.Conv2d(in_features, out_features, kernel_size=3, padding=pad),
+                         nn.BatchNorm2d(out_features),
+                         nn.ReLU(),
+                         nn.Conv2d(out_features, out_features, kernel_size=3, padding=pad),
+                         nn.BatchNorm2d(out_features),
+                         nn.ReLU())
+
+
+def _upsample_block(in_features, out_features):
+    """Returns a transposed convolutional layer, followed by a batch norm and ReLU activation"""
+    return nn.Sequential(nn.ConvTranspose2d(in_features, out_features, kernel_size=2, stride=2),
+                         nn.BatchNorm2d(out_features),
+                         nn.ReLU())
+
 
 class UNET(nn.Module):
     def __init__(self, n_classes, padding='valid'):
@@ -26,81 +151,100 @@ class UNET(nn.Module):
         self.padding = padding
 
         # ENCODING PHASE
-        self.conv1 = conv_x2_block(3, 64, padding)
-        self.conv2 = conv_x2_block(64, 128, padding)
-        self.conv3 = conv_x2_block(128, 256, padding)
-        self.conv4 = conv_x2_block(256, 512, padding)
-        self.conv5 = conv_x2_block(512, 1024, padding)
-        
+        self.conv1 = _conv_x2_block(3, 64, padding)
+        self.conv2 = _conv_x2_block(64, 128, padding)
+        self.conv3 = _conv_x2_block(128, 256, padding)
+        self.conv4 = _conv_x2_block(256, 512, padding)
+        self.conv5 = _conv_x2_block(512, 1024, padding)
+
         # DECODING PHASE
-        self.upsample6 = upsample_block(1024, 512)
+        self.upsample6 = _upsample_block(1024, 512)
         # Concat here
-        self.conv6 = conv_x2_block(1024, 512, padding)
-        
-        self.upsample7 = upsample_block(512, 256)
+        self.conv6 = _conv_x2_block(1024, 512, padding)
+
+        self.upsample7 = _upsample_block(512, 256)
         # Concat here
-        self.conv7 = conv_x2_block(512, 256, padding)
-        
-        self.upsample8 = upsample_block(256, 128)
+        self.conv7 = _conv_x2_block(512, 256, padding)
+
+        self.upsample8 = _upsample_block(256, 128)
         # Concat here
-        self.conv8 = conv_x2_block(256, 128, padding)
-        
-        self.upsample9 = upsample_block(128, 64)
+        self.conv8 = _conv_x2_block(256, 128, padding)
+
+        self.upsample9 = _upsample_block(128, 64)
         # Concat here
-        self.conv9 = conv_x2_block(128, 64, padding)
-        
+        self.conv9 = _conv_x2_block(128, 64, padding)
+
         # Classification layer
         self.classifier = nn.Conv2d(64, n_classes, kernel_size=1)
-        
+
         # Reused layers
         self.pool = nn.MaxPool2d(kernel_size=2)
         self.dropout = nn.Dropout(p=.25)
-        
+
     def forward(self, x):
-        
+
         # ENCODING PHASE
         x1 = self.conv1(x)
         x = self.pool(x1)
         x = self.dropout(x)
-        
+
         x2 = self.conv2(x)
         x = self.pool(x2)
         x = self.dropout(x)
-        
+
         x3 = self.conv3(x)
         x = self.pool(x3)
         x = self.dropout(x)
-        
+
         x4 = self.conv4(x)
         x = self.pool(x4)
         x = self.dropout(x)
-        
+
         # BRIDGE
         x = self.conv5(x)
-        
+
         # DECODING PHASE
         x = self.upsample6(x)
-        
+
         x = self.dropout(x)
         x = torch.cat([x, centercrop(x4, size=x.shape[2:])], dim=1)
         x = self.conv6(x)
-        
+
         x = self.upsample7(x)
         x = self.dropout(x)
         x = torch.cat([x, centercrop(x3, size=x.shape[2:])], dim=1)
         x = self.conv7(x)
-        
+
         x = self.upsample8(x)
         x = self.dropout(x)
         x = torch.cat([x, centercrop(x2, size=x.shape[2:])], dim=1)
         x = self.conv8(x)
-        
+
         x = self.upsample9(x)
         x = self.dropout(x)
         x = torch.cat([x, centercrop(x1, size=x.shape[2:])], dim=1)
         x = self.conv9(x)
-        
+
         # Classification layer
         x = self.classifier(x)
-        
+
         return x
+
+    def load_state_dict(self, state_dict):
+        """
+        Models that were trained on parallel GPUs need this special workaround so they can be used on single GPU/CPU.
+        """
+        try:
+
+            super().load_state_dict(state_dict)
+            print('this worked')
+        except:
+            try:
+                new_state_dict = OrderedDict()
+                for k, v in state_dict.items():
+                    name = k[7:]
+                    new_state_dict[name] = v
+                super().load_state_dict(new_state_dict)
+                print('take 2 worked')
+            except:
+                raise ValueError("Unable to load weights to model.")
